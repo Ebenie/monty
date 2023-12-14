ffb8bab62788055792a954bbe35e18781fea8fc8
Ebenie <abrahamavenue163@.com>
2023-12-14 06:40:29 -0800

    0x19. C - Stacks, Queues - LIFO, FIFO

---
+++ monty.h
@@ -0,0 +1,19 @@
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+        int n;
+        struct stack_s *prev;
+        struct stack_s *next;
+} stack_t;
+
+void pint(int line_number);
+void push(int value, int line_number);
+void pall(int line_number);
